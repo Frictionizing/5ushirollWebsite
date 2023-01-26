3637b2e62bccc25461c3f5cb76eca6b1c8cdb9d2
Brian CJ Chu <57924309+Frictionizing@users.noreply.github.com>
2023-01-26 15:46:25 -0800

    Import React project To repository
    First Commit !

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Card from './components/card.components';
+
+function App() {
+  return (
+    <div className="App">
+      <Card title ={"pussy"} description = {"fuck"}/>
+      <Card title ={"pusssussy"} description = {"fuc"}/>       
+    </div>
+  );
+}
+
+export default App;
